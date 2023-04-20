ba8a76d9e5deebf6e620792f35f3883a0dc2f41b
Rikuto <orikutobasuke@gmail.com>
2023-04-20 10:44:26 +0900

    ReactCSS

--- src/pages/components/FirstView.tsx
+++ src/pages/components/FirstView.tsx
@@ -3,7 +3,9 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 export default function FirstView() {
     return (
         <>
+            <Box w={'100%'} h={'100vh'} bg={'skyblue'}>
 
+            </Box>
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navigation from './components/Navigation'
+import FirstView from './components/FirstView'
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <Box position={'relative'}>
         <Navigation />
+        <FirstView />
       </Box>
 
     </>
