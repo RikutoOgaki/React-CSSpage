a34c7a7ae0226aa6f2da2319359c772c2d5b86c5
Rikuto <orikutobasuke@gmail.com>
2023-04-20 12:15:05 +0900

    ReactCSS

--- src/pages/components/FirstView.tsx
+++ src/pages/components/FirstView.tsx
@@ -7,17 +7,7 @@ export default function FirstView() {
             <Box bg={'skyblue'} marginLeft={'25rem'} paddingTop={'8vh'}>
                 <Box margin={'0 3rem'} padding={'2rem'}>
                     <Box as='section'>
-                        <Text
-                            as={'h3'}
-                            fontSize={'5xl'}
-                        >ReactでのCSSの使い方</Text>
-                        <Box as='pre' bg={'#fff'}>
-                            <Box as='code'>
-                                <Text>
 
-                                </Text>
-                            </Box>
-                        </Box>
                     </Box>
                 </Box>
             </Box >
