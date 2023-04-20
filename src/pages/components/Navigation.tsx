01c932a5a56c1e73f4ce43c6facb29b9f0c50a4a
Rikuto <orikutobasuke@gmail.com>
2023-04-20 09:48:24 +0900

    ReactCSS

--- src/pages/components/Navigation.tsx
+++ src/pages/components/Navigation.tsx
@@ -1,7 +1,26 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, List, OrderedList, ListItem } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function Navigation() {
     return (
-        <></>
+        <>
+            <Box>
+                <Flex
+                    as='aside'
+                >
+                    <Link href={'/'}>
+
+                    </Link>
+                    <nav>
+                        <OrderedList>
+                            <ListItem></ListItem>
+                            <ListItem></ListItem>
+                            <ListItem></ListItem>
+                            <ListItem></ListItem>
+                        </OrderedList>
+                    </nav>
+                </Flex>
+            </Box>
+        </>
     )
 }
