808b183958a308844a08a3a14abb22bb3ed76226
Rikuto <orikutobasuke@gmail.com>
2023-04-20 19:00:48 +0900

    ReactCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navigation from './components/Navigation'
-import FirstView from './components/FirstView'
+// import FirstView from './components/FirstView'
 import Header from './components/Head'
 
 
@@ -16,7 +16,7 @@ export default function Home() {
       <Box position={'relative'} overflow={'hidden'}>
         <Header />
         <Navigation />
-        <FirstView />
+        {/* <FirstView /> */}
       </Box>
 
     </>
