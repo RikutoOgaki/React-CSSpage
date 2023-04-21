4f041f3be646c9669f888cca96482e16023285a9
Rikuto <orikutobasuke@gmail.com>
2023-04-21 12:12:00 +0900

    ReactCSS

--- src/pages/components/parts/cssparts/sample02.tsx
+++ src/pages/components/parts/cssparts/sample02.tsx
@@ -2,6 +2,17 @@ import style from './css/sample02.module.css'
 
 export default function Sample02() {
     return (
-        <></>
+        <>
+            <div className={style.wrap}>
+                <div className={style.ex}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </>
     )
 }
