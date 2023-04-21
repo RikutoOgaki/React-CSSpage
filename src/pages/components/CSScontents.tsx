6c25340bc953538d7f5093a4ac56aae047d585b9
Rikuto <orikutobasuke@gmail.com>
2023-04-21 15:23:33 +0900

    ReactCSS

--- src/pages/components/CSScontents.tsx
+++ src/pages/components/CSScontents.tsx
@@ -8,16 +8,13 @@ export default function CSS() {
             <Flex
                 justifyContent={'center'}
                 alignItems={'center'}
-                marginLeft={'25rem'}
                 paddingTop={'8vh'}
             >
                 <Flex
                     flexDir={'column'}
                     justifyContent={'center'}
                     alignItems={'flex-start'}
-                    gap={'10rem'}
-                    margin={'0 3rem'}
-                    padding={'2rem'}>
+                    gap={'10rem'}>
                     <Box as='section'>
                         <Text fontSize={'7xl'}>React + CSSについて</Text>
                         <Text fontSize={'3xl'}>
@@ -25,20 +22,20 @@ export default function CSS() {
                             なので多く語ることは正直ない。
                         </Text>
                     </Box>
-                    <Box as='section'>
+                    <Box
+                        w={'100%'}
+                        as='section'>
                         <Text fontSize={'7xl'}>1 . Drop Down Menu</Text>
-                        <Box marginTop={'10rem'} height={'25rem'}>
-                            <Drop />
-                        </Box>
-                        <Box
-                            w={'100vh'}
-                            h={'100rem'}
-                            as='iframe'
-                            src='https://codesandbox.io/s/xenodochial-sun-l6tcqo'
-                            title="distracted-cori-x6d0s"
-                            allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-                            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-                        >
+                        <Box w={'100%'} h={'50rem'}>
+                            <Box
+                                w={'110%'}
+                                h={'50rem'}
+                                as='iframe'
+                                src="https://codesandbox.io/embed/xenodochial-sun-l6tcqo?fontsize=14&hidenavigation=1&theme=dark"
+                                title="xenodochial-sun-l6tcqo"
+                                allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                                sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+                            ></Box>
                         </Box>
                     </Box>
                 </Flex >
