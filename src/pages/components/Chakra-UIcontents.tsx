d76c5274ca99977674589e341dc4812fed5cdd9d
Rikuto <orikutobasuke@gmail.com>
2023-04-25 10:03:43 +0900

    ReactCSS

--- src/pages/components/Chakra-UIcontents.tsx
+++ src/pages/components/Chakra-UIcontents.tsx
@@ -33,13 +33,48 @@ export default function Chakra() {
                             </Text>
                             の一つで自分でCSSを書かなくてもスタイルを記述することができて、<br />
                             デザインに一貫性をもたせることができる。<br />
+                            専用のタグやレスポンシブを効かせたページを作ることがとても楽にできます。<br />
+                            BootstrapやTailwind cssなどもございますが、そちらとの比較は別ページで行います。<br />
+                            随時更新していきますのでお待ち下さい。
                         </Text>
                     </Box>
-                    <Box as='section' w={'90rem'}>
-                        <Text fontSize={'7xl'}>1 . Hamburger Menu</Text>
-                        <Box w={'100%'} h={'50rem'}>
-
+                    <Box as='section'>
+                        <Text fontSize={'7xl'}>Chakra-UIのここがすごい！</Text>
+                        <Box paddingTop={'3rem'}>
+                            <Text fontSize={'5xl'} paddingBottom={'2rem'}>1 .  タグの中にCSSプロパティを書くことができる！</Text>
+                            <Text fontSize={'3xl'}>
+                                やはりタグだけでページを作ろうと思うと、限界がありしたいこともできません。<br />
+                                そこでChakra-UIはプロパティをあてたいタグの中に書くことができます。<br />
+                                下記のように書けばスタイルをあてることができます！
+                            </Text>
+                            <Box
+                                as='iframe'
+                            ></Box>
                         </Box>
+                    </Box>
+                    <Box as='section'>
+                        <Text fontSize={'7xl'} marginBottom={'2rem'}> 1 . ＜Box＞</Text>
+                        <Text fontSize={'3xl'}>
+                            HTMLでいいうところの
+                            <Box as='code' color={'red.500'}>
+                                ＜div＞
+                            </Box>のようなもの。<br />
+                            特によく使うタグの一つだと私は考えます。
+                        </Text>
+                    </Box>
+                    <Box as='section'>
+                        <Text fontSize={'7xl'} marginBottom={'2rem'}>2 . ＜Flex＞</Text>
+                        <Text fontSize={'3xl'}>
+                            こちらのタグも、
+                            <Box as='code' color={'red.500'}>
+                                ＜div＞
+                            </Box>のように使うことが可能です。<br />
+                            CSS、Sass/Scssを書いたことがある人はわかると思います。<br />
+                            display:flex;でスタイルを当てることが多いです。<br />
+                            なんとこのタグ１つでこのタグの中にある子要素はすべて横並びになるのです。<br />
+                            なので中央揃えや、等間隔に開けるなどが簡単にできてしまうのです。<br />
+                            このタグを私は一番使います。
+                        </Text>
                     </Box>
                 </Flex>
             </Flex>
